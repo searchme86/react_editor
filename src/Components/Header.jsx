41a74c4d54558ea852ac280b4f66472b49b26868
searchme86 <searchme86@gmail.com>
2023-04-06 16:15:19 +0900

    페이지 헤더 수정

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Logo } from '../assets/images/logo.svg';
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 48px;
+  background: #2c3240;
+`;
+
+const HeaderLeftSection = styled.div`
+  display: flex;
+`;
+
+const HeaderCenterSection = styled.div``;
+const HeaderRightSection = styled.div``;
+
+const HeaderLogo = styled.div`
+  width: 48px;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function Header() {
-  const testStyle = {
-    backgroundColor: 'yellow'
-  };
   return (
-    <header>
-      <div className="" style={testStyle}>
-        <div className="logo">로고</div>
+    <HeaderContainer>
+      <HeaderLeftSection>
+        <HeaderLogo>
+          <Logo width="15" height="15" />
+        </HeaderLogo>
         <div className="">
-          <p>여기에는 인풋바</p>
+          <select>
+            <option>ddd</option>
+          </select>
         </div>
-        <div className="right">버튼모음</div>
-      </div>
-    </header>
+      </HeaderLeftSection>
+      <HeaderCenterSection></HeaderCenterSection>
+      <HeaderRightSection>버튼, 버튼,버튼</HeaderRightSection>
+    </HeaderContainer>
   );
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,175 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavContainer = styled.nav`
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 function Navbar() {
-  const testStyle = {
-    backgroundColor: 'blue'
-  };
-
   return (
-    <div style={testStyle}>
-      <div className="">
-        <ul>
-          <li>홈</li>
-          <li>문서</li>
-          <li>상품</li>
-          <li>템플릿</li>
-          <li>조직</li>
-        </ul>
-      </div>
-      <div className="">
-        <ul>
-          <li>유저등록</li>
-          <li>환경설정</li>
-        </ul>
-      </div>
-    </div>
+    <NavContainer>
+      <ul>
+        <li>
+          <Link to="/">
+            <div className="">
+              {/* 이미지 */}
+              <span>홈</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="">
+              {/* 이미지 */}
+              <span>문서</span>
+            </div>
+          </Link>
+          <div className="subMenu">
+            <div className="">
+              {/* 이미지 */}
+              <span>기초서류관리</span>
+            </div>
+            <ul>
+              <li>
+                <Link to="/document/management">
+                  <div className="">
+                    {/* 이미지 */}
+                    <span>문서관리!!!! </span>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <div className="">
+                    {/* 이미지 */}
+                    <span>문서작성편집</span>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>문서일괄편집</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>맞춤형 약관</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <Link to="/">
+            <span>상품</span>
+          </Link>
+          <div className="subMenu">
+            <div className=""></div>
+            <ul>
+              <li>
+                <Link to="/">
+                  <span>상품 정보관리</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>상품 분류관리</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>상품 항목관리</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="">
+              {/* 이미지 */}
+              <span>문서</span>
+            </div>
+          </Link>
+          <div className="subMenu">
+            <div className=""></div>
+            <ul>
+              <li>
+                <Link to="/">
+                  <span>특약관리</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>분류표 관리</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>서식관리</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>표준약관관리</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <Link to="/">
+            <span>조직</span>
+          </Link>
+          <div className="subMenu">
+            <div className=""></div>
+            <ul>
+              <li>
+                <Link to="/">
+                  <span>조직텍스트 </span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>조직텍스트 </span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>조직텍스트 </span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <span>조직텍스트 </span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/">
+            <div className="">
+              {/* 이미지 */}
+              <span>유저등록</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="">
+              {/* 이미지 */}
+              <span>환경설정</span>
+            </div>
+          </Link>
+        </li>
+      </ul>
+    </NavContainer>
   );
 }
 
