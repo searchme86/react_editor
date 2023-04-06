5f489adf75121024f16215fdda81aac72fb4a62d
searchme86 <searchme86@gmail.com>
2023-04-06 16:14:56 +0900

    프로젝트 글로벌 스타일 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import GlobalStyle from './Assets/Styles/GlobalStyle';
+import GlobalStyle from './assets/styles/GlobalStyle';
 
 import { BrowserRouter } from 'react-router-dom';
 
